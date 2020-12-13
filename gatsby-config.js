1a1b74d50d2870be67ad5f5c6a211fc23a8f8192
Yussif <djiceman.gh@gmail.com>
2020-12-13 01:29:37 +0000

    header and video background

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,12 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Yussifweb Travel Site`,
+    description: `Lets Help you travel around the world.`,
     author: `@gatsbyjs`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-source-filesystem`,
       options: {
